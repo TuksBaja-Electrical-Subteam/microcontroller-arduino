ab1c0034eb60180fabf2b6a29a732fdf9c864794
AbbeyPie <u24030776@tuks.co.za>
2025-09-18 16:29:15 +0200

    Basic Test Code for the LCD

--- microcontroller-arduino.ino
+++ microcontroller-arduino.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <LCD-I2C.h>
+#include <Wire.h>
+LCD_I2C lcd(0x27, 16, 2);
+int sensorRead;
+int lim = 409;
+int count = 0;
 
+void setup() 
+{
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+  Wire.begin();
+  lcd.begin(&Wire);
+  lcd.display();
+  lcd.backlight();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
 
+  sensorRead = analogRead(A0);
+  if(sensorRead < 409)
+  {
+    count = count + 1;
+    lcd.print(count);
+    Serial.print(" ");
+    Serial.print(count);
+  }
+  delay(160);
 }
+
