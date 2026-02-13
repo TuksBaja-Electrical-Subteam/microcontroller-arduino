81dff709ec44f7d2176b4583a21f8ec150d556b4
Sagarika Dash <109913251+IcyNoble37@users.noreply.github.com>
2026-02-13 13:59:38 +0200

    basic dash- it1
    - calcs speed using hall switch (change)
    - starts the web dashboard
    - measures on GPIO18 for interrupts

---
+++ baja-dashboard/baja-main-dashboard/main/main.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/pulse_cnt.h"
+#include "driver/gpio.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "esp_mac.h"
+#include "web_server.h"
+
+#define HALL_SENSOR_GPIO 18
+#define WHEEL_CIRCUMFERENCE 1.0         //change this to actual measurement
+#define WIFI_SSID      "BAJA_dashboard"
+#define WIFI_PASS      "baja2026" 
+
+pcnt_unit_handle_t pcnt_unit = NULL;
+float current_speed = 0.0;
+
+void init_pcnt(void) {
+    pcnt_unit_config_t unit_config = {
+        .high_limit = 2000,
+        .low_limit = -2000,
+    };
+    ESP_ERROR_CHECK(pcnt_new_unit(&unit_config, &pcnt_unit));
+
+    pcnt_chan_config_t chan_config = {
+        .edge_gpio_num = HALL_SENSOR_GPIO,
+        .level_gpio_num = -1, // Not used
+    };
+    pcnt_channel_handle_t pcnt_chan = NULL;
+    ESP_ERROR_CHECK(pcnt_new_channel(pcnt_unit, &chan_config, &pcnt_chan));
+
+    pcnt_glitch_filter_config_t filter_config = { .max_glitch_ns = 1000 };
+    ESP_ERROR_CHECK(pcnt_unit_set_glitch_filter(pcnt_unit, &filter_config));
+    ESP_ERROR_CHECK(pcnt_channel_set_edge_action(pcnt_chan, PCNT_CHANNEL_EDGE_ACTION_INCREASE, PCNT_CHANNEL_EDGE_ACTION_HOLD));
+
+    ESP_ERROR_CHECK(pcnt_unit_enable(pcnt_unit));
+    ESP_ERROR_CHECK(pcnt_unit_clear_count(pcnt_unit));
+    ESP_ERROR_CHECK(pcnt_unit_start(pcnt_unit));
+}
+
+void speed_calc_task(void* pvParameters) {
+    int pulses = 0;
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+        ESP_ERROR_CHECK(pcnt_unit_get_count(pcnt_unit, &pulses));
+        ESP_ERROR_CHECK(pcnt_unit_clear_count(pcnt_unit));
+
+        current_speed = (pulses * WHEEL_CIRCUMFERENCE) * 3.6;
+    }
+}
+
+void wifi_init_softap(void) {
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_ap();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    wifi_config_t wifi_config = {
+        .ap = {
+            .ssid = WIFI_SSID,
+            .ssid_len = strlen(WIFI_SSID),
+            .password = WIFI_PASS,
+            .max_connection = 4,
+            .authmode = WIFI_AUTH_WPA2_PSK,
+            .channel = 1,
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+void app_main(void) {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    init_pcnt();
+    wifi_init_softap();
+    start_webserver();
+
+    xTaskCreate(speed_calc_task, "speed_calc", 2048, NULL, 5, NULL);
+}
